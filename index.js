4025571d0abf1057f7e2e304d15bfcfc131d391a
Infant Frontender <newbornfrontender@gmail.com>
2019-01-09 21:50:36 +0300

    Add sample from Using The Console

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert('Hello world!');
+console.time('Test');
+
+const resolveAfter2Seconds = (x) =>
+  new Promise((res) => setTimeout(() => res(x), 2000));
+
+const add = async (x) => {
+  const a = resolveAfter2Seconds(40);
+  const b = resolveAfter2Seconds(25);
+
+  return x + (await a) + (await b);
+};
+
+console.log('Spam 1');
+console.log('Spam 1');
+console.log('Spam 1');
+console.log('Spam 1');
+
+console.table({ a: 1, b: 2 });
+console.table([1, 2, 3]);
+
+console.warn('Warn!!!');
+
+add(15).then((v) => console.log(v));
+
+console.log('Spam 2');
+console.log('Spam 2');
+console.log('Spam 2');
+console.log('Spam 2');
+
+console.timeEnd('Test');
